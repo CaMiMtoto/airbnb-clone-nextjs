fa52c618f56439836b92f58080a70618c5dece74
Jean Paul Byiringiro <jeanpaulcami@live.com>
2021-12-08 18:46:30 +0200

    first commit

---
+++ components/MediumCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Image from "next/image";
+
+function MediumCard({ img, title }) {
+  return (
+    <div>
+      <div className="relative h-80 w-80 hover:scale-105 transform transition duration-300 ease-out">
+        <Image src={img} layout="fill" className="rounded-xl" />
+      </div>
+      <h3 className="text-2xl mt-3">{title}</h3>
+    </div>
+  );
+}
+
+export default MediumCard;
